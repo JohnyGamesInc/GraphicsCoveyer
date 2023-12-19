59edd9643dafcea7365c5668ac40cb1787c5fbb2
Ivanov Evgenii <ivanjohnoff@gmail.com>
2023-12-19 19:06:06 +0300

    custom scriptable render pipeline

---
+++ Assets/GraphicPipeline/CustomRP/SpaceRunPipelineRenderer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+
+namespace GraphicPipeline.CustomRP
+{
+    
+    
+    public class SpaceRunPipelineRenderer : RenderPipeline
+    {
+
+        private CameraRenderer _cameraRenderer = new();
+        
+        
+        protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+        {
+            CamerasRender(context, cameras);
+        }
+
+
+        private void CamerasRender(ScriptableRenderContext context, Camera[] cameras)
+        {
+            foreach (var camera in cameras)
+            {
+                _cameraRenderer.Render(context, camera);
+            }
+        }
+
+
+    }
+}
